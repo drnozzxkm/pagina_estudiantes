9f265031483a53526eb01845d8a8665f3c10bd13
drnozzxkm <drno.zzxkm1@gmail.com>
2025-10-03 04:29:54 -0500

    Transfiriendo todos los datos

--- app.py
+++ app.py
@@ -183,9 +183,9 @@ def quiz_result(course_slug, topic_slug):
 
 
 if __name__ == '__main__':
-    app.run(debug=True)
-
-
+    import os
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
 
 
 
